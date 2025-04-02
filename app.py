e684161264ada2a440c65ed489dfde3b27fc2f21
christina078 <christymerin078@gmail.com>
2025-04-02 12:30:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,102 @@
 #importing required libraries
-
-from flask import Flask, request, render_template
-import numpy as np
+import streamlit as st
 import pandas as pd
-from sklearn import metrics 
-import warnings
+import numpy as np
 import pickle
-warnings.filterwarnings('ignore')
-from feature import FeatureExtraction
 
-file = open("pickle/model.pkl","rb")
-gbc = pickle.load(file)
-file.close()
+# Load Model and Scaler
+with open('xgmodel.pkl', 'rb') as file:
+    xgmodel = pickle.load(file)
+
+with open('scl.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+st.header('BANK CUSTOMER CHURN PREDICTION')
+with st.sidebar:
+    st.image("https://dezyre.gumlet.io/images/blog/machine-learning-case-studies/Customer_Churn_Prediction_Machine_Learning_Case_Study.png?w=940&dpr=1.3")
+    st.subheader('About this project')
+    st.write('This application predicts whether a bank customer churns or not.')
+
+# User inputs
+CreditScore = st.number_input('Enter Credit Score:', min_value=0, max_value=1000, value=600)
+Geography = st.selectbox('Choose the region:', ('France', 'Spain', 'Germany'))
+Gender = st.selectbox('Gender:', ('Male', 'Female'))
+Age = st.slider('Enter the Age:', 18, 100, 30)
+Tenure = st.number_input('Enter Tenure:', min_value=0, max_value=10, value=5)
+NumOfProducts = st.slider('No of Products purchased:', 1, 5, 2)
+HasCrCard = st.selectbox('Does the customer have Credit card?', ('Yes', 'No'))
+IsActiveMember = st.selectbox('Is the customer an active member?', ('Yes', 'No'))
+EstimatedSalary_f = st.selectbox('Estimated salary', ('0-50K', '50K-100K', '100K-150K', '150K-200K'))
+Complain = st.selectbox('Has complained?', ('Yes', 'No'))
+Satisfaction_Score = st.selectbox("Satisfaction Score", (1, 2, 3, 4, 5))
+Cardtype = st.selectbox('Card Type:', ('DIAMOND', 'SILVER', 'GOLD', 'PLATINUM'))
+Point_earned = st.number_input('Points Earned:', min_value=0, value=1000)
+Balance_cat = st.selectbox('Balance Category', ('Zero', '0-50K', '50K+'))
+
+# Encoding categorical variables
+EstimatedSalary_0to50K, EstimatedSalary_50Kto100K, EstimatedSalary_100Kto150K, EstimatedSalary_150Kto200K = 0, 0, 0, 0
+if EstimatedSalary_f == '0-50K':
+    EstimatedSalary_0to50K = 1
+elif EstimatedSalary_f == '50K-100K':
+    EstimatedSalary_50Kto100K = 1
+elif EstimatedSalary_f == '100K-150K':
+    EstimatedSalary_100Kto150K = 1
+else:
+    EstimatedSalary_150Kto200K = 1
+
+Gender = 0 if Gender == 'Female' else 1
+HasCrCard = 1 if HasCrCard == 'Yes' else 0
+Complain = 1 if Complain == 'Yes' else 0
+IsActiveMember = 1 if IsActiveMember == 'Yes' else 0
+
+Geography_France, Geography_Germany, Geography_Spain = 0, 0, 0
+if Geography == 'France':
+    Geography_France = 1
+elif Geography == 'Germany':
+    Geography_Germany = 1
+else:
+    Geography_Spain = 1
+
+Card_Type_Gold, Card_Type_Platinum, Card_Type_Silver, Card_Type_Diamond = 0, 0, 0, 0
+if Cardtype == 'GOLD':
+    Card_Type_Gold = 1
+elif Cardtype == 'DIAMOND':
+    Card_Type_Diamond = 1
+elif Cardtype == 'PLATINUM':
+    Card_Type_Platinum = 1
+else:
+    Card_Type_Silver = 1
+
+Balance_Category_Zero, Balance_Category_0to50K, Balance_Category_50Kabove = 0, 0, 0
+if Balance_cat == 'Zero':
+    Balance_Category_Zero = 1
+elif Balance_cat == '0-50K':
+    Balance_Category_0to50K = 1
+else:
+    Balance_Category_50Kabove = 1
+
+#scaling
+numerical_features = np.array([[CreditScore, Age, Tenure, NumOfProducts, Satisfaction_Score, Point_earned]])
+scaled_features = scaler.transform(numerical_features)[0] 
+
+CreditScore, Age, Tenure, NumOfProducts, Satisfaction_Score, Point_earned = scaled_features
 
 
-app = Flask(__name__)
+features = [CreditScore, Gender, Age, Tenure, NumOfProducts, HasCrCard, IsActiveMember, Complain, 
+            Satisfaction_Score, Point_earned, Balance_Category_0to50K, Balance_Category_50Kabove, 
+            Balance_Category_Zero, Geography_France, Geography_Germany, Geography_Spain, 
+            Card_Type_Gold, Card_Type_Platinum, Card_Type_Silver, Card_Type_Diamond, 
+            EstimatedSalary_0to50K, EstimatedSalary_100Kto150K, EstimatedSalary_150Kto200K, 
+            EstimatedSalary_50Kto100K]
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
+# Prediction
+if st.button('Predict'):
+    input_data = np.array(features).reshape(1, -1)
+    prediction = xgmodel.predict(input_data)[0]
+    
+    # Displaying the Result
+    if prediction == 1:
+        st.error("This customer is likely to churn.")
+    else:
+        st.success("This customer is not likely to churn.")
 
-        url = request.form["url"]
-        obj = FeatureExtraction(url)
-        x = np.array(obj.getFeaturesList()).reshape(1,30) 
-
-        y_pred =gbc.predict(x)[0]
-        #1 is safe       
-        #-1 is unsafe
-        y_pro_phishing = gbc.predict_proba(x)[0,0]
-        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
-        # if(y_pred ==1 ):
-        pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
-    return render_template("index.html", xx =-1)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
